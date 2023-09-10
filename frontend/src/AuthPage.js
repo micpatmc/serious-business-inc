f479fc9cb1e7e427c642133979d73252e5ae3a5c
dukeyeom <dukedyeom@gmail.com>
2023-09-10 19:21:56 -0400

    POST requests have been set up

--- frontend/src/AuthPage.js
+++ frontend/src/AuthPage.js
@@ -3,7 +3,8 @@ import {
   useState
 } from 'react';
 import axios from 'axios';
-import stockArt from './images/loginPageArt.jpg'
+import stockArt from './images/loginPageArt.jpg';
+const baseUrl = 'http://seriousbusinessincorporated.online/LAMPAPI';
 
 const AuthPage = (props) => {
   return (
@@ -97,9 +98,6 @@ const ToggleBar = ({loginOn, setLoginOn}) => {
     >
       Register
     </button>
-    {/* <button onClick={() => setLoginOn(!loginOn)}>
-      Toggle
-    </button> */}
   </div>);
 };
 
@@ -119,13 +117,20 @@ const LoginCard = () => {
   }
 
   const handleSubmit = e => {
-    axios.post('')
+    e.preventDefault();
+    console.log('Request object:', credentials);
+    axios.post(
+      `${baseUrl}/Login.php`,
+      credentials)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 
   return (
   <div>
     <h2>Welcome back</h2>
     <form
+      onSubmit={e => handleSubmit(e)}
       style={{
         display: "flex",
         flexDirection: "column"
@@ -147,35 +152,80 @@ const LoginCard = () => {
       </label>
       <input
         type="submit"
-        onSubmit={e => handleSubmit(e)}
       />
     </form>
   </div>);
 };
 
 const RegistrationCard = () => {
-  // function submitData {
-  //   const submitObject ={
-  //     // fill in based on POST api
-  //     username: username,
-  //     password: password,
-  //     name: name,
-  //     email: email
-  //   }
-  //   axios.post('group8awesomeproject.com/Login.php', submitObject);
-  //   .then((response) => {
-  //     switch (response.data.code) {
-  //       case 200:
-  //         Yay! Switch page
-  //       case 401:
-  //         display error message
-  //     }
-  //   });
-  // }
-  return (<>
+  const [newUser, setNewUser] = useState({
+    userId: '',
+    password: '',
+    FirstName: '',
+    LastName: ''
+  });
+
+  const handleChange = (e, key) => {
+    setNewUser({
+      ...newUser,
+      [key]: e.target.value
+    });
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('Request object:', newUser);
+    axios.post(
+      `${baseUrl}/AddUser.php`,
+      newUser)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+  }
+
+  return (
+  <div>
     <h2>Create a new account</h2>
-    <p>Registration</p>
-  </>);
+    <form
+      onSubmit={e => handleSubmit(e)}
+      style={{
+        display: "flex",
+        flexDirection: "column"
+      }}
+    >
+      <label>
+        First Name
+        <input
+          value={newUser.FirstName}
+          onChange={e => handleChange(e, 'FirstName')}
+        />
+      </label>
+      <label>
+        Last Name
+        <input
+          value={newUser.LastName}
+          onChange={e => handleChange(e, 'LastName')}
+        />
+      </label>
+      <hr />
+      <label>
+        Username
+        <input
+          value={newUser.userId}
+          onChange={e => handleChange(e, 'userId')}
+        />
+      </label>
+      <label>
+        Password
+        <input
+          value={newUser.password}
+          onChange={e => handleChange(e, 'password')}
+        />
+      </label>
+      <input
+        type="submit"
+      />
+    </form>
+  </div>);
 };
 
 export default AuthPage;
