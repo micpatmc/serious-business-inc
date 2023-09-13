8e4374511457a63924b5bc5eafbfa8d0fe2f1ae7
Zandroid505 <zakariaantifit@hotmail.com>
2023-09-13 17:38:02 -0400

    Centered elements on login page, changed image, and removed in line styles.

--- frontend/src/AuthPage.js
+++ frontend/src/AuthPage.js
@@ -1,94 +1,55 @@
-import {
-  React,
-  useState
-} from 'react';
-import axios from 'axios';
-import stockArt from './images/loginPageArt.jpg';
-import { useNavigate } from 'react-router-dom';
-const baseUrl = 'http://seriousbusinessincorporated.online/LAMPAPI';
+import { React, useState } from "react";
+import "./AuthPage.css"
+import axios from "axios";
+import stockArt from "./images/login_page.jpg";
+import { useNavigate } from "react-router-dom";
+const baseUrl = "http://seriousbusinessincorporated.online/LAMPAPI";
 
 const AuthPage = () => {
   return (
-    <div
-      style={{
-        // width: "100vw",
-        // height: "100vh",
-        backgroundColor: "white"
-      }}
-    >
-      {/*Inner box holding AuthBox and side image.*/}
+      /*Inner box holding AuthBox and side image.*/
       <div
-        style={{
-          height: "80vh",
-          margin: "10%",
-          boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr",
-        }}
+          id="AuthPage-home"
+          className="grid text-center rounded shadow-lg p-3 mb-5 bg-body-tertiary rounded"
       >
-          <AuthBox />
-          <div>
+        <div className="row align-items-center">
+          <AuthBox className="col"/>
+          <div className="col">
             {/* Picture. */}
-            <img src={stockArt}
+            <img
+              src={stockArt}
+              className="img-fluid"
               alt="Generic business pic."
-              style={{
-                objectFit: "fill",
-                boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-                // Below attributes helps keep size same when scaling browser.
-                maxWidth: "100%",
-                maxHeight: "100%",
-                // height: "auto"
-              }}>
-              </img>
+            ></img>
           </div>
+        </div>
       </div>
-    </div>
   );
-}
+};
 
 const AuthBox = () => {
   const [loginOn, setLoginOn] = useState(true);
 
-  const authState = (loginOn)
-    ? <LoginCard />
-    : <RegistrationCard />;
+  const authState = loginOn ? <LoginCard /> : <RegistrationCard />;
   return (
+    // Login/Register box.
     <div
-      style={{
-        height: "75vh",
-        display: "flex",
-        justifyContent:'center', 
-        alignItems:'center'
-      }}
-      >
-    {/* Sign in box. */}
-    <div
-      width="50%"
-      style={{
-        boxShadow: "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
-        borderRadius: "9px",
-        width: "75%",
-        maxWidth: "400px",
-        height: ""   
-      }}
+      id="AuthPage-home-authbox"
+      className="col shadow-lg p-3 mb-5 bg-body-tertiary rounded"
     >
-      <ToggleBar 
-        loginOn={loginOn}
-        setLoginOn={setLoginOn}
-      />
+      <ToggleBar loginOn={loginOn} setLoginOn={setLoginOn} />
       {authState}
-    </div>
     </div>
   );
 };
 
-const ToggleBar = ({loginOn, setLoginOn}) => {
+const ToggleBar = ({ loginOn, setLoginOn }) => {
   const buttonStyle = {
     width: "50%",
     height: "3rem",
     border: "0rem",
     borderRadius: "9px 9px 0 0",
-    fontSize: "1.2rem"
+    fontSize: "1.2rem",
   };
   const onStyle = {
     ...buttonStyle,
@@ -99,20 +60,21 @@ const ToggleBar = ({loginOn, setLoginOn}) => {
     backgroundColor: "lightgray",
   };
   return (
-  <div>
-    <button
-      style={loginOn ? onStyle : offStyle}
-      onClick={() => setLoginOn(true)}
-    >
-      Login
-    </button>
-    <button
-      style={loginOn ? offStyle : onStyle}
-      onClick={() => setLoginOn(false)}
-    >
-      Register
-    </button>
-  </div>);
+    <div>
+      <button
+        style={loginOn ? onStyle : offStyle}
+        onClick={() => setLoginOn(true)}
+      >
+        Login
+      </button>
+      <button
+        style={loginOn ? offStyle : onStyle}
+        onClick={() => setLoginOn(false)}
+      >
+        Register
+      </button>
+    </div>
+  );
 };
 
 // WARNING PSUEDOCODE AHEAD
@@ -121,148 +83,145 @@ const LoginCard = () => {
   const navigate = useNavigate();
 
   const [credentials, setCredentials] = useState({
-    login: '',
-    password: ''
+    login: "",
+    password: "",
   });
 
   const handleChange = (e, key) => {
     setCredentials({
       ...credentials,
-      [key]: e.target.value
+      [key]: e.target.value,
     });
-  }
+  };
 
   const [errorMsg, setErrorMsg] = useState({
-    msg: ''
+    msg: "",
   });
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     // What is this?
     e.preventDefault();
-    console.log('Request object:', credentials);
-    axios.post(
-      `${baseUrl}/Login.php`,
-      credentials)
-      .then(response => {
-        if ((response.data.error === 'No Records Found') && 
-              (credentials.login.length !== 0 || 
-                credentials.password).length !== 0) {
+    console.log("Request object:", credentials);
+    axios
+      .post(`${baseUrl}/Login.php`, credentials)
+      .then((response) => {
+        if (
+          response.data.error === "No Records Found" &&
+          (credentials.login.length !== 0 || credentials.password).length !== 0
+        ) {
           console.log(response);
           console.log(response.data.error);
           setErrorMsg({
-            msg: 'No records with those credentials.'});
-        }
-        else {
-          sessionStorage.setItem('currentUser', JSON.stringify(response.data))
-          navigate('/contacts');
+            msg: "No records with those credentials.",
+          });
+        } else {
+          sessionStorage.setItem("currentUser", JSON.stringify(response.data));
+          navigate("/contacts");
         }
       })
-      .catch(error => console.log(error));
-  }
+      .catch((error) => console.log(error));
+  };
 
   return (
-  <div>
-    <h2>Welcome back</h2>
-    <form
-      onSubmit={e => handleSubmit(e)}
-      style={{
-        display: "flex",
-        flexDirection: "column"
-      }}
-    >
-      <label>
-        Username
-        <input
-          value={credentials.login}
-          onChange={e => handleChange(e, 'login')}
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={credentials.password}
-          onChange={e => handleChange(e, 'password')}
-        />
-      </label>
-      <input
-        type="submit"
-      />
-    </form>
-    <h1 style={{color:"red"}}>{errorMsg.msg}</h1>
-  </div>);
+    <div>
+      <h2>Welcome back</h2>
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <label>
+          Username
+          <input
+            value={credentials.login}
+            onChange={(e) => handleChange(e, "login")}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            value={credentials.password}
+            onChange={(e) => handleChange(e, "password")}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+      <h1 style={{ color: "red" }}>{errorMsg.msg}</h1>
+    </div>
+  );
 };
 
 const RegistrationCard = () => {
   const [newUser, setNewUser] = useState({
-    userId: '',
-    password: '',
-    FirstName: '',
-    LastName: ''
+    userId: "",
+    password: "",
+    FirstName: "",
+    LastName: "",
   });
 
   const handleChange = (e, key) => {
     setNewUser({
       ...newUser,
-      [key]: e.target.value
+      [key]: e.target.value,
     });
-  }
+  };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Request object:', newUser);
-    axios.post(
-      `${baseUrl}/AddUser.php`,
-      newUser)
-      .then(response => console.log(response))
-      .catch(error => console.log(error));
-  }
+    console.log("Request object:", newUser);
+    axios
+      .post(`${baseUrl}/AddUser.php`, newUser)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
 
   return (
-  <div>
-    <h2>Create a new account</h2>
-    <form
-      onSubmit={e => handleSubmit(e)}
-      style={{
-        display: "flex",
-        flexDirection: "column"
-      }}
-    >
-      <label>
-        First Name
-        <input
-          value={newUser.FirstName}
-          onChange={e => handleChange(e, 'FirstName')}
-        />
-      </label>
-      <label>
-        Last Name
-        <input
-          value={newUser.LastName}
-          onChange={e => handleChange(e, 'LastName')}
-        />
-      </label>
-      <hr />
-      <label>
-        Username
-        <input
-          value={newUser.userId}
-          onChange={e => handleChange(e, 'userId')}
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={newUser.password}
-          onChange={e => handleChange(e, 'password')}
-        />
-      </label>
-      <input
-        type="submit"
-      />
-    </form>
-  </div>);
+    <div>
+      <h2>Create a new account</h2>
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <label>
+          First Name
+          <input
+            value={newUser.FirstName}
+            onChange={(e) => handleChange(e, "FirstName")}
+          />
+        </label>
+        <label>
+          Last Name
+          <input
+            value={newUser.LastName}
+            onChange={(e) => handleChange(e, "LastName")}
+          />
+        </label>
+        <hr />
+        <label>
+          Username
+          <input
+            value={newUser.userId}
+            onChange={(e) => handleChange(e, "userId")}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            value={newUser.password}
+            onChange={(e) => handleChange(e, "password")}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default AuthPage;
