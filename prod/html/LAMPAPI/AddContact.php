6416adbfcf417772fd0537372b2b66b4f3747261
Caleb Freckmann <91574624+tokyorecon@users.noreply.github.com>
2023-09-16 19:17:29 -0500

    Contact.php new

--- prod/html/LAMPAPI/AddContact.php
+++ prod/html/LAMPAPI/AddContact.php
@@ -3,65 +3,67 @@
 $inData = getRequestInfo();
 
 $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-if ($conn->connect_error) 
-{
-    returnWithError( $conn->connect_error );
-} 
-else
-{
-    $firstName = $inData["FirstName"];
-    $lastName = $inData["LastName"];
-    $emailAddress = $inData["Email"];
-    $phoneNumber = $inData["Phone"];
-    $userId = $inData["UserID"];
 
-    // Check for duplicates
-    $stmt = $conn->prepare("SELECT * FROM Contact WHERE Email=? AND UserID=?");
-    $stmt->bind_param("ss", $emailAddress, $userId);
-    $stmt->execute();
-    $result = $stmt->get_result();
+//  check connections
+/*if($conn->conn_error)
+{
+  returnWithError("Connection failed: ");
+}*/
 
+//else
+//{
+
+  //$sql ="INSERT INTO contact_list (id, first_name, last_name, phone, email) VALUES('34','TRIET', 'BRYAN', 1234567890, 'Toawesomeforthisproject@gmail.com' , 'NULL')";
+  $sql = "INSERT INTO `Contact` (id, firstName, lastName, phoneNumber, emailAddress, userId) VALUES('" . $inData["ID"] . "', '" . $inData["FirstName"] . "', '" . $inData["LastName"] . "', '" . $inData["Phone"] . "', '" . $inData["Email"] . "', '" . $inData["UserID"] . "')";
+  $sql2 = "SELECT cid FROM Contact where id='". $inData["ID"] . "' and firstName = '" . $inData["FirstName"] . "' and lastName = '" . $inData["LastName"] . "' and phoneNumber = '" . $inData["Phone"] . "' and emailAddress = '" . $inData["Email"] .  "' and userId = '" . $inData["UserID"] . "'";
+  if($conn->query($sql) === TRUE)
+  {
+    $result = $conn->query($sql2);
     if($result->num_rows > 0)
     {
-        $stmt->close();
-        $conn->close();
-        returnWithError("Duplicate contact found.");
+      //echo "New record created successfully";
+      $row = $result->fetch_assoc();
+      $cid = $row["cid"];
+      //echo $cid ;
+      $id = $inData["ID"];
+      $firstName = $inData["FirstName"];
+      $lastName = $inData["LastName"];
+      $phoneNumber = $inData["Phone"];
+      $emailAddress = $inData["Email"];
+      $userId = $inData["UserID"];
+      $conn->close();
     }
-    else
-    {
-        // Insert the contact
-        $stmt = $conn->prepare("INSERT INTO Contact (FirstName, LastName, Email, Phone, UserID) VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssss", $firstName, $lastName, $emailAddress, $phoneNumber, $userId);
-        $stmt->execute();
 
-        $stmt->close();
-        $conn->close();
+    returnWithInfo($id,$cid, $firstName, $lastName, $phoneNumber, $emailAddress, $userId);
 
-        returnWithInfo("Contact added successfully.");
-    }
-}
+  }
+  else
+  {
+      returnWithError("Contact Creation failure");
+  }
+
+
+
+//}
 
 function getRequestInfo()
 {
-    return json_decode(file_get_contents('php://input'), true);
+  return json_decode(file_get_contents('php://input'), true);
 }
-
 function sendResultInfoAsJson( $obj )
 {
-    header('Content-type: application/json');
-    echo $obj;
+  header('Content-type: application/json');
+  echo $obj;
 }
-
 function returnWithError( $err )
 {
-    $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-    sendResultInfoAsJson( $retValue );
+  $retValue = '{"id":"0","firstName":"","lastName":"","phone":"","email":"","address":"","error":"' . $err . '"}';
+  sendResultInfoAsJson( $retValue );
 }
-
-function returnWithInfo( $info )
+function returnWithInfo( $id, $cid, $firstName, $lastName, $phoneNumber, $emailAddress, $userId)
 {
-    $retValue = '{"results":' . json_encode($info) . ',"error":""}';
-    sendResultInfoAsJson( $retValue );
+  $retValue = '{"id":"'. $id . '","cid":"'. $cid . '","FirstName":"' . $firstName . '","LastName":"' . $lastName . '","Phone":"'. $phoneNumber . '","Email":"' . $emailAddress . '","UserID":"' . $userId . '","error":""}';
+  sendResultInfoAsJson( $retValue );
 }
 
-?>
+>
