0a36c3282508954580504890a1409676b563bbd2
dukeyeom <dukedyeom@gmail.com>
2023-09-16 23:49:20 -0400

    Working on integrating contact apis into frontend

---
+++ prod/html/LAMPAPI/SearchContacts.php
@@ -0,0 +1,76 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$id = $inData["userId"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$phoneNumber = $inData["phone"];
+	$emailAddress = $inData["email"];
+	
+	$conn = new mysqli("localhost", "Itachi", "!COP4331isthebest", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("SELECT firstName, lastName FROM Table WHERE firstName LIKE='?' AND lastName LIKE='?'");
+		$firstName = $inData["FirstName"];
+    	$lastName = $inData["LastName"];
+		
+		$stmt->bind_param("sssss", $id, $firstName, $lastName, $phoneNumber, $emailAddress);
+		$stmt->execute();
+		
+		$result = $stmt->get_result();
+		
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .= '"' . $row["Name"] . '"';
+		}
+		
+		if( $searchCount == 0 )
+		{
+			returnWithError( "No Records Found" );
+		}
+		else
+		{
+			returnWithInfo( $searchResults );
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
