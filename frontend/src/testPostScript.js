c7f295d33a86b2c81fd7574a3862d839785440ef
dukeyeom <dukedyeom@gmail.com>
2023-09-18 15:25:55 -0400

    added corporate memphis image

--- frontend/src/testPostScript.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const baseUrl = 'http://seriousbusinessincorporated.online/LAMPAPI';
-
-// whwhite58
-// BlueSky99.1%
-//
-
-axios.post(`${baseUrl}/DeleteContacts.php`, {userId: currentUser.id, [id]})
