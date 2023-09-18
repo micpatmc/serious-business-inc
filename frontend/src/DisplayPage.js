ed91fb137a138e07fac1fe4d753ee7d7d51141aa
Zandroid505 <zakariaantifit@hotmail.com>
2023-09-18 12:08:47 -0400

    Added some padding to inner box on landing page.

--- frontend/src/DisplayPage.js
+++ frontend/src/DisplayPage.js
@@ -23,38 +23,35 @@ const DisplayPage = () => {
           ></img>
         </div>
       </nav>
-      <div className="container">
-        <div
-          id="DisplayPage-centerbox"
-          className="row align-items-center grid rounded shadow-lg m-5"
-        >
-          <div className="col">
-            {/* Mission Statement */}
-            <h1>Welcome to our site!</h1>
-            <h2>Serious Business Inc.</h2>
-            <h3>Contact Manager</h3>
-            <p id="DisplayPage-missionstatement">
-              Serious Business Incorporated integrates cutting-edge business
-              solutions to drive growth. Stakeholder engagement remains
-              paramount in our market-centric approach. Through strategic
-              alignment and robust value propositions, industry benchmarks
-              consistently evolve.
-            </p>
-            <button 
-              className="btn btn-secondary m-3"
-              onClick={goToTargetPage}>
-              Login/Registration
-            </button>
-          </div>
-          <div className="col">
-            {/* <img
+      {/* <div className="container align-items-center"> */}
+      <div
+        id="DisplayPage-centerbox"
+        className="row align-items-center grid rounded shadow-lg m-5 p-3"
+      >
+        <div className="col m-3">
+          {/* Mission Statement */}
+          <h1>Welcome to our site!</h1>
+          <h2>Serious Business Inc.</h2>
+          <h3>Contact Manager</h3>
+          <p id="DisplayPage-missionstatement">
+            Serious Business Incorporated integrates cutting-edge business
+            solutions to drive growth. Stakeholder engagement remains paramount
+            in our market-centric approach. Through strategic alignment and
+            robust value propositions, industry benchmarks consistently evolve.
+          </p>
+          <button className="btn btn-secondary m-3" onClick={goToTargetPage}>
+            Login/Register
+          </button>
+        </div>
+        <div className="col">
+          {/* <img
               className="img-fluid"
               src={businessCartoon}
               alt="Cartoon people disucssing business."
             ></img> */}
-          </div>
         </div>
       </div>
+      {/* </div> */}
     </div>
   );
 };
